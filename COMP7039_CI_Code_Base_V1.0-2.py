5d45a8272e0ffb8dbbe8f7abc2b9e28194d886fb
Sam Roche <Sam.roche@mycit.ie>
2023-11-13 15:13:49 +0000

    Fixed Spelling mistake on function

--- COMP7039_CI_Code_Base_V1.0-2.py
+++ COMP7039_CI_Code_Base_V1.0-2.py
@@ -7,7 +7,7 @@ def read_integer_between_numbers(prompt, mini, maximum):
             else:
                 print(f"Numbers from {mini} to {maximum} only.")
         except ValueError:
-            print("Sorry, numbers only please")
+            print("Sorry -numbor olny please")
 
 
 def read_nonempty_string(prompt):
@@ -25,7 +25,7 @@ def read_integer(prompt):
             if users_input >= 0:
                 return users_input
         except ValueError:
-            print("Sorry -numbor olny please")
+            print("Sorry, numbers only please")
 
 
 def runners_data():
