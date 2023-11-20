022287d4870ad9fdfde1754ec05541ea2b6f5675
Daniel Dennehy <daniel.dennehy1@mycit.ie>
2023-11-20 15:34:38 +0000

    WIP: [final debug]

--- COMP7039_CI_Code_Base_V1.0-2.py
+++ COMP7039_CI_Code_Base_V1.0-2.py
@@ -2,7 +2,7 @@ def read_integer_between_numbers(prompt, mini, maximum):
     while True:
         try:
             users_input = int(input(prompt))
-            if maximum <= users_input >= mini:
+            if maximum >= users_input >= mini:
                 return users_input
             else:
                 print(f"Numbers from {mini} to {maximum} only.")
@@ -47,7 +47,7 @@ def runners_data():
 
 def race_results(races_location):
     for i in range(len(races_location)):
-        print(f"{i}: {races_location[i]}")
+        print(f"{i + 1}: {races_location[i]}")
     user_input = read_integer_between_numbers("Choice > ", 1, len(races_location))
     venue = races_location[user_input - 1]
     id, time_taken = reading_race_results(venue)
